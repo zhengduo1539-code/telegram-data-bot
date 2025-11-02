332039336de62163f6c76a959ce6e390f0a46d4c
zhengduo1539-code <zhengduo1539@gmail.com>
2025-11-03 04:09:18 +0630

    Update main.py

--- main.py
+++ main.py
@@ -556,10 +556,4 @@ def main():
     application.run_polling(poll_interval=1.0)
 
 if __name__ == '__main__':
-    try:
-        from web_server import keep_alive
-        keep_alive()
-    except ImportError:
-        pass
-
     main()
