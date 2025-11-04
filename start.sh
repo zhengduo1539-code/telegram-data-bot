66f087979faa3c5f507c6d5c5e7a414802bb24ec
zhengduo1539-code <zhengduo1539@gmail.com>
2025-11-05 03:46:34 +0630

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python -c "import main; main.main()"
-
-python -c "import asyncio, main; asyncio.run(main.setup_bot(main.application))"
-
-echo "Starting Gunicorn..."
-gunicorn main:app --workers 1 --bind 0.0.0.0:$PORT
